8f652bb5da525ad051372e71a16f1a73176a763b
Salomón Sanchez <salomonpz14@gmail.com>
2025-05-30 12:03:36 -0500

    Configure workflow Feature Push Notification

---
+++ src/main/java/com/bootcamp/github/actions/BootcampGitHubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.github.actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampGitHubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampGitHubActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/github/actions/BootcampGitHubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.github.actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampGitHubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
